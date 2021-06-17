c2db0f8d748c29aa7e1fc5ca9266649f4b45b4b4
vassilyev <vassilyev@toowto.kz>
2021-06-17 21:26:04 +0600

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,8 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswortHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        
     }
 }
