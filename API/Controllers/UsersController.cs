5a74ea4d5ed4e0d51cadcd8eae3cbbb013880ff8
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-06-25 02:24:58 +0600

    Part 8. 1 - 7 lesson

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,27 +9,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
-    public class UsersController : BaseApiController
+  [Authorize]
+  public class UsersController : BaseApiController
+  {
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        {
-            return await _context.Users.ToListAsync();
-        }
-
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            return await _context.Users.FindAsync(id);
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
+  }
 }
